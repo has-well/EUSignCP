7fbe55868af53e223ccce2d63d5cd5d16e1f5f4f
kazanzhy <dkazanzhy@gmail.com>
2020-02-18 23:20:29 +0200

    added API

---
+++ api.py
@@ -0,0 +1,69 @@
+import io
+
+from flask import Flask, request, jsonify, send_file, url_for
+from EUSignCP import EULoad, EUUnload, EUGetInterface
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    resp = 'Entry poins for binary POST:<br>'
+    resp += request.host + url_for('GetSignsCount') + '<br>'
+    resp += request.host + url_for('GetSignerInfo') + '<br>'
+    resp += request.host + url_for('GetDataFromSignedData') + '<br>'
+    return resp
+
+
+@app.route('/GetSignsCount', methods=['POST'])
+def GetSignsCount():
+    signedbin = request.stream.read()
+    EULoad()
+    intF = EUGetInterface()
+    intF.Initialize()
+    signs_count = []
+    try:
+        intF.GetSignsCount(None, signedbin, len(signedbin), signs_count)
+    except Exception as e:
+        return jsonify(e)
+    intF.Finalize()
+    EUUnload()
+    return jsonify(signs_count)
+
+
+@app.route('/GetSignerInfo', methods=['POST'])
+def GetSignerInfo():
+    signedbin = request.stream.read()
+    EULoad()
+    intF = EUGetInterface()
+    intF.Initialize()
+    signer_info = {}
+    try:
+        intF.GetSignerInfo(0, None, signedbin, len(signedbin), signer_info, None)
+    except Exception as e:
+        return jsonify(e)
+    intF.Finalize()
+    EUUnload()
+    return jsonify(signer_info)
+
+
+@app.route('/GetDataFromSignedData', methods=['POST'])
+def GetDataFromSignedData():
+    signedbin = request.stream.read()
+    EULoad()
+    intF = EUGetInterface()
+    intF.Initialize()
+    unsignedbin = []
+    try:
+        intF.GetDataFromSignedData(None, signedbin, len(signedbin), unsignedbin)
+    except Exception as e:
+        return jsonify(e)
+    intF.Finalize()
+    EUUnload()
+    return send_file(io.BytesIO(unsignedbin[0]), mimetype='application/octet-stream')
+
+
+
+app.run(host='0.0.0.0', port=5005, debug=True)
+
--- test.py
+++ test.py
@@ -1,34 +1,17 @@
-import json
-import base64
-from EUSignCP import EULoad, EUUnload, EUGetInterface
-
-with open('Modules/cert/CAs.json') as cert:
-    certs = json.load(cert)
-
-binfile = open('3188718600R19K2A.xlsx.p7s', 'rb').read()
-infile = open('signed.pdf', 'rb').read()
-
-EULoad()
-inf = EUGetInterface()
-inf.Initialize()
-
-try:
-
-    signs = []
-    #inf.GetSignsCount(pszSign=None, pbSign=binfile, dwSignLength=len(binfile), pdwCount=signs)
-    inf.GetFileSignsCount(pszFileNameWithSign='statut.pdf', pdwCount=signs)
-    print('Signs:', signs)
-    signer = []
-    cert = []
-    inf.GetFileSignerInfo(dwSignIndex=0, pszFileNameWithSign='3188718600R19K2A.xlsx.p7s', ppInfo=signer, ppbCertificate=None)
-    #signer = base64.b64decode(signer[0])
-    print(signer)
-    print(cert)
-finally:
-    inf.Finalize()
-
-EUUnload()
+import requests
+from pathlib import Path
 
 
+filename = '3188718600R19K2A.xlsx.p7s'
+with open(filename, 'rb') as f:
+    filebin = f.read()
 
-#
+r = requests.post('http://localhost:5005/GetSignsCount', data=filebin)
+print(r.json())
+
+r = requests.post('http://localhost:5005/GetSignerInfo', data=filebin)
+print(r.json())
+
+r = requests.post('http://localhost:5005/GetDataFromSignedData', data=filebin)
+Path(filename[:-4]).write_bytes(r.content)
+print(r.status_code)
