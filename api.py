f858a3792ecdb97206e7c7e12b9577b4e6bf5d55
Dmytro Kazanzhy <dkazanzhy@gmail.com>
2020-02-19 12:21:50 +0200

    Last

--- api.py
+++ api.py
@@ -32,18 +32,7 @@ def GetSignsCount():
     try:
         intF.GetSignsCount(None, signedbin, len(signedbin), signs_count)
     except Exception as e:
-        return jsonify([repr(e)])
-    #Test
-    tmp = {}
-    intF.GetFileStoreSettings(pszPath=tmp)
-    print(tmp)
-    #Test
-    cert = {}
-    intF.EnumOwnCertificates(0, cert)
-    print(cert)
-    print()
-    for c in EU_CERTIFICATES: print(c)
-    #test
+        return jsonify({'Error': repr(e)})
     intF.Finalize()
     EUUnload()
     return jsonify(signs_count)
@@ -62,7 +51,7 @@ def GetSignerInfo():
     try:
         intF.GetSignerInfo(0, None, signedbin, len(signedbin), signer_info, None)
     except Exception as e:
-        return jsonify([repr(e)])
+        return jsonify({'Error': repr(e)})
     intF.Finalize()
     EUUnload()
     return jsonify(signer_info)
@@ -81,7 +70,7 @@ def GetDataFromSignedData():
     try:
         intF.GetDataFromSignedData(None, signedbin, len(signedbin), unsignedbin)
     except Exception as e:
-        return jsonify([repr(e)])
+        return jsonify({'Error': repr(e)})
     intF.Finalize()
     EUUnload()
     return send_file(io.BytesIO(unsignedbin[0]), mimetype='application/octet-stream')
