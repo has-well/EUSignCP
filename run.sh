13b3f4ab1430fd4378c64c1871e4a248a2bce173
Dmytro Kazanzhy <dkazanzhy@gmail.com>
2020-02-19 09:22:43 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export LD_LIBRARY_PATH="$PWD/Modules"
 
-[ ! -d "venv" ] && virtualenv -p python3 venv
+[ ! -d "venv" ] && sudo python3 -m pip install virtualenv && virtualenv -p python3 venv
 
 source venv/bin/activate
 
@@ -9,3 +9,5 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 gunicorn -D -w 1 -b 0.0.0.0:5005 api:app
+
+#ps aux | grep gunicorn | awk '{ print $2 }' | xargs kill -9
